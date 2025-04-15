9b3bd26249b679e428a4f37a2880ed701df8238b
Ehtesham <ehteshamkhan2002@gmail.com>
2025-04-15 18:58:23 +0530

    change connectionstring

--- createAdmin.js
+++ createAdmin.js
@@ -1,24 +1,26 @@
 // createAdmin.js
 const mongoose = require('mongoose');
-const Admin = require('./models/Admin');  
+const Admin = require('./models/Admin');
 
-mongoose.connect("mongodb://127.0.0.1:27017/the-awaz", {
+mongoose.connect("mongodb+srv://danishkhan705010:1S3rnh1jeaHNMS5W@cluster0.cq9pxoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(async () => {
-  console.log("database is connected");
-  try {
-    const admin = new Admin({ name: "new admin", password: "987654321" });
-    await admin.save();
-    console.log("Admin user created successfully");
-  } catch (err) {
-    console.error("Error creating admin:", err);
-  } finally {
-    process.exit(0);
-  }
-})
-.catch((err) => {
-  console.log("database is not connected");
-  console.error(err);
-});
+  .then(async () => {
+    console.log("database is connected");
+    try {
+      const admin = new Admin({ name: "new admin", password: "987654321" });
+      await admin.save();
+      console.log("Admin user created successfully");
+    } catch (err) {
+      console.error("Error creating admin:", err);
+    } finally {
+      process.exit(0);
+    }
+  })
+  .catch((err) => {
+    console.log("database is not connected");
+    console.error(err);
+  });
+
+
--- db/conn.js
+++ db/conn.js
@@ -1,19 +1,45 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/the-awaz", {
+// mongoose.connect("mongodb+srv://<danishkhan705010>:<1S3rnh1jeaHNMS5W>@cluster0.mongodb.net/the-awaz?retryWrites=true&w=majority", {
 
-    useNewUrlParser: true,
+//     useNewUrlParser: true,
 
-    useUnifiedTopology: true
+//     useUnifiedTopology: true
 
-}).then(() => {
+// }).then(() => {
 
-    console.log("database is connected")
+//     console.log("database is connected")
 
-}).catch((err) => {
+// }).catch((err) => {
 
-    console.log("database is not connected");
+//     console.log("database is not connected");
 
-    console.log(err);
+//     console.log(err);
 
-})
+// })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://danishkhan705010:1S3rnh1jeaHNMS5W@cluster0.cq9pxoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
